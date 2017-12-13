c0e2291a2c3b1cbe6ea5d2dda4ad643fae3ebd58
Ulisses Maia <ulissesmaia1311@outlook.com>
2017-12-13 09:17:12 -0200

    13-12-2017

---
+++ TDDnaPratica/ConverteNumerosRomanos/ConverteNumerosRomanos/ConversorDeNumeroRomano.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverteNumerosRomanos
+{
+   
+    public class ConversorDeNumeroRomano
+    {
+
+
+        public int Converte(string numeroRomano)
+        {
+            return 0;
+        }
+
+      
+    }
+}
---
+++ TDDnaPratica/ConverteNumerosRomanos/ConverteNumerosRomanos/ConversorDeNumeroRomanoTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace ConverteNumerosRomanos
+{
+    [TestFixture]
+    public class ConversorDeNumeroRomanoTest
+    {
+        [Test]
+        public void DeveEntenderOSimboloI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("I");
+            Assert.AreEqual(1, numero);
+         }
+    }
+}
